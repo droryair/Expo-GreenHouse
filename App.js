60b60fb15dcafad44f43cb1e18efc040d5850541
drorya2324@gmail.com <drorya2324@gmail.com>
2020-10-28 13:31:00 +0200

    added MyGarden component

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ImageList from './src/container/ImageList';
+import { Provider } from 'mobx-react';
+import Plants from './Stores/Plants'
+import Home from './Components/Home'
+
+// additional needed components:
+// Plant- render each plant to "MyGarden" component
+// navbar - menue component
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+
+      <Provider store={store}>
+        <Plants />
+      </Provider>
     </View>
   );
 }
@@ -19,3 +35,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
