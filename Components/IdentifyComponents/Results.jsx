fdcdd11e39d70ade2d71b27b44bf825efa4fa4b9
ziv221 <ziv.itzh@gmail.com>
2020-11-01 22:33:40 +0200

    added image after finding data

--- Components/IdentifyComponents/Results.jsx
+++ Components/IdentifyComponents/Results.jsx
@@ -1,30 +1,22 @@
 import { observer } from 'mobx-react';
-import { toJS } from 'mobx';
 import * as React from 'react';
 import {useContext} from 'react';
-import { Button, StyleSheet, View, TouchableOpacity, Text, TextInput, Image} from 'react-native';
+import { Button, StyleSheet, View, Text, Image} from 'react-native';
 import { PlantsContext } from '../../App';
 
   const Results = observer(({ navigation }) => {
     const plantsStore = useContext(PlantsContext)
-    const printStore = () =>{
-        console.log(toJS(plantsStore.identification.plantData))
-        console.log(plantsStore.identification.image)
-        console.log(plantsStore.identification.wikiData)
-    }
     return ( 
         <View style={styles.container}>
-            <Text style={styles.header}> Search Result</Text>
-            <TouchableOpacity style={styles.takePicture} onPress={() => navigation.navigate("PlantIdentify")} >
-                <Text style={styles.buttonText}> Back </Text>
-            </TouchableOpacity>
             <Button title="Menu" onPress={() => navigation.toggleDrawer()} />
-            <Button title="Print Store" onPress={printStore} />   
-            <Text style={styles.header}>{plantsStore.identification.identified.plant_name}</Text>   
-            {/* <Image style={{ width: 100, height: 100 }} source={`${plantsStore.identification.image}`} /> */}
-            <Image style={{ width: 100, height: 100 }} source="https://cdn.pixabay.com/photo/2020/02/20/18/58/funnel-web-spider-4865535_960_720.jpg" />
-            <Text>{plantsStore.identification.wikiData}</Text>   
-
+            <Text style={styles.header}>{plantsStore.identification.identified.plant_name}</Text>
+            <Image
+                style={styles.plantImage}
+                source={{
+                uri: plantsStore.identification.image,
+                }}
+            />
+            <Text>{plantsStore.identification.wikiData}</Text>
         </View>
     )
 })
@@ -35,9 +27,13 @@ const styles = StyleSheet.create({
         justifyContent: 'center' 
     },
     header:{
-        fontSize: 40,
+        fontSize: 20,
         color:"black",
         marginBottom: 50
+    },
+    plantImage: {
+        width: 100,
+        height: 200,
     }
 })
 
