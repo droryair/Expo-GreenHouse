5a5f23af364a00ad8c4d000f2470016939555e00
tzlilhaim <tzlilhaim480@gmail.com>
2020-11-02 22:11:43 +0200

    Added routes and functions templates

--- server/botanist/botanist.js
+++ server/botanist/botanist.js
@@ -1,12 +1,32 @@
 const controller = require("../../db/controllers/controller")
+const getRecommendations = require("./scraping/scrapePlantsRecommendation")
+// Can use "./enteties/Disease"
 
 const getDiseaseInfo = async (diseaseId) => {
   // Write code
   // return data
+  // controller.botanist.getDiseaseById....
 }
 const getAllDiseasesOfPlant = async (plantId) => {
   // Write code
   // return data
+  // controller.diseases.getDiseasesByPlant....
+}
+const getPlantByConditions = async (conditions) => {
+  // Write code
+  // return data
+  // const recommendedPlants = await getRecommendations.byConditions(conditions)
 }
 
-module.exports = { getDiseaseInfo, getAllDiseasesOfPlant }
+const getPlantsByType = async (conditions) => {
+  // Write code
+  // return data
+  // const recommendedPlants = await getRecommendations.byPlantType(conditions)
+}
+
+module.exports = {
+  getDiseaseInfo,
+  getAllDiseasesOfPlant,
+  getPlantByConditions,
+  getPlantsByType,
+}
