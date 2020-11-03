5d6311a596e274befde736cad74b555277db40ba
ziv221 <ziv.itzh@gmail.com>
2020-11-03 23:45:13 +0200

    added display of plants

--- Components/GardenComponents/PlantDetails.jsx
+++ Components/GardenComponents/PlantDetails.jsx
@@ -1,29 +1,44 @@
 import { NavigationHelpersContext } from '@react-navigation/native';
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View, ScrollView } from 'react-native';
 import { usePlantsStore } from '../../App';
+import { Card, Icon } from 'react-native-elements';
+import plantIcon from '../../assets/plant.png'
 
 // this component will be responsible to determine a plant details format
 // and rendering them from a given plant details object
 
-
-
-
-
 export default function PlantDetails(props){
-    const Plantstore = usePlantsStore()
-    const allPlants = Object.values(Plantstore.PlantsStore.plantsArr)
-    const plantID = props.route.params.plantID
-    const navigation = props.navigation
-    const plant = allPlants.find(p=>p.id===plantID)
-    console.log(plant)
+    const store         = usePlantsStore(),
+          {serverUrl}   = store.utilityStore,
+          {plantID}     = props.route.params,
+          navigation    = props.navigation,
+          [plantData, setPlantData] = useState({}), 
+          [conditions, setConditions] = useState([])
+
+
+    const getPlantDetails = () =>{
+        fetch(`${serverUrl}:3001/plant/${plantID}`, {
+            method: 'GET'
+            })
+            .then(response => response.json())
+            .then(responseJson => {
+                setPlantData(responseJson[0])
+                setConditions(responseJson[0].conditions.map(c => ({name:c.name , value:c.value})))
+            })
+            .catch(err => {
+                console.log(err);
+                alert(err)
+            })
+    }
+
+    useEffect(()=>{
+        getPlantDetails()
+    },[])
 
     const notifyWatering=()=>{
         navigation.navigate('ScheduleNotifications', {plant})
     }
-
-
-
 
     return (
         <View
@@ -32,14 +47,32 @@ export default function PlantDetails(props){
                 justifyContent: "center",
                 alignItems: "center"
             }}>
-            <Button title="Menu" onPress={() => navigation.toggleDrawer()} />
-
-            <Text>Plant Details</Text>
-            <Text>{plant.nickname? plant.nickname : plant.name}</Text>
-            <Button
-                title="notify watering"
-                onPress={notifyWatering}
-            />
+             <Button title="Menu" onPress={() => navigation.toggleDrawer()} />
+            <Card>
+                <Text>Name: {plantData.nickname}</Text>
+                <Text>Scientific Name: {plantData.scientific_name}</Text>
+                <Text>Watering Frequency: {plantData.watering_frequency}</Text>
+                <Card.Divider />
+                <Text style={{ marginBottom: 10 }}>
+                </Text>
+                <ScrollView>
+                    <Text>Growing Conditions</Text>
+                    {conditions.map(c => <Text>{c.name} - {c.value} </Text> )}
+                </ScrollView>
+                <Button
+                    title="notify watering"
+                    onPress={notifyWatering}
+                /> 
+            </Card>
+            
         </View>
+        
     )
 }
+
+const styles = StyleSheet.create({
+    tinyLogo: {
+        width: 50,
+        height: 50,
+    },
+});
