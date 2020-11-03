60c68604484e349c3c044df7678adc74e6692586
tzlilhaim <tzlilhaim480@gmail.com>
2020-11-03 02:44:46 +0200

    Added navigation & search

--- Components/GardenComponents/NewPlant.jsx
+++ Components/GardenComponents/NewPlant.jsx
@@ -1,16 +1,95 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from "react"
+import axios from "axios"
+import { Text, View, Button } from "react-native"
+import { Input } from "react-native-elements"
+import Icon from "react-native-vector-icons/FontAwesome"
+import LoadingState from "./../UtilityComponents/LoadingState"
+import EmptyState from "./../UtilityComponents/EmptyState"
 
-export default function NewPlant(){
+export default function NewPlant() {
+  const [isSearched, setIsSearched] = useState(false)
+  const [inputs, setInputs] = useState({ search: "" })
+  const [isLoading, setIsLoading] = useState(false)
+  const [selectedPlant, setSelectedPlant] = useState(null)
+  const [searchResults, setSearchResults] = useState([])
+  const serverUrl = "http://10.0.0.4:3001"
 
-    return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
-            <Text>New Plant</Text>
-        </View>
+  const handleSearch = async () => {
+    try {
+      setIsLoading(true)
+      const searchResults = await axios.get(
+        `${serverUrl}/plantForm/${inputs.search}`
+      )
+      setSearchResults(searchResults.data)
+      setIsLoading(false)
+      setIsSearched(true)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleResultPress = async (event) => {
+    const plantName = event.target.innerHTML
+    const p = searchResults.find((p) => p.name === plantName)
+    console.log(p)
+    const plant = await axios.get(
+      `${serverUrl}/plantForm/${plantName}/info`,
+      p.detailsUrl
     )
+    setSelectedPlant(plant.data ? plant.data : null)
+  }
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>
+        <div className="new-plant-form">
+          {isLoading ? (
+            <LoadingState />
+          ) : isSearched ? (
+            selectedPlant ? (
+              <div className="plant-info">
+                plant details
+                <Button title="Add Plant" />
+              </div>
+            ) : (
+              <div className="search-results">
+                {searchResults.length ? (
+                  searchResults.map((result, index) => (
+                    <Button
+                      key={`result-${index}`}
+                      title={result.name}
+                      onPress={handleResultPress}
+                    />
+                  ))
+                ) : (
+                  <EmptyState />
+                )}
+              </div>
+            )
+          ) : (
+            <div className="search-plant">
+              <Input
+                placeholder="Plant name"
+                value={inputs.search}
+                onChangeText={(value) =>
+                  setInputs({ ...inputs, search: value })
+                }
+                leftIcon={<Icon name="search" size={24} color="black" />}
+              />
+              <Button
+                title="Search"
+                onPress={handleSearch}
+                disabled={inputs.search === ""}
+              />
+            </div>
+          )}
+        </div>
+      </Text>
+    </View>
+  )
 }
--- Components/GardenComponents/RenderPlant.jsx
+++ Components/GardenComponents/RenderPlant.jsx
@@ -1,61 +1,83 @@
-import React from 'react';
-import { Button, Text, View, Image, ScrollView, StyleSheet } from 'react-native';
-import { Card, Icon } from 'react-native-elements';
-import plantIcon from '../../assets/plant.png'
+import React from "react"
+import { Button, Text, View, Image, ScrollView, StyleSheet } from "react-native"
+import { Card, Icon } from "react-native-elements"
+import plantIcon from "../../assets/plant.png"
 // this component will be responsible to determine a plant format
 // and rendering each plant from a given array
 export default function RenderPlant(props) {
-    console.log("render plants")
-    const plants = props.route.params.plants
-    const navigation = props.navigation
-    const area = props.route.params.area
-    console.log(area);
-    const handlePress = (plant) => {
-        return (
-            navigation.navigate('PlantDetails', { plant })
-        )
-    }
-    return (
-        <ScrollView>
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                <Button title="Menu" onPress={() => navigation.toggleDrawer()} />
-                <Text>These are the plants in your -{area.nickName ? area.nickName : area.type}- garden area</Text>
-                <View>
-                    {plants.map((p, i) => {
-                        return (
-                            <Card key={i}>
-                                <Text>
-                                    <Card.Title>id: {p}</Card.Title>
-                                </Text>
-                                <Card.Divider />
-                                <Image
-                                    style={styles.tinyLogo}
-                                    source={p.imgURL ? p.imgURL : plantIcon}
-                                />
-                                <Text style={{ marginBottom: 10 }}>
-                                </Text>
-                                <Button
-                                    icon={<Icon name='code' color='green' />}
-                                    buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
-                                    title='View Plant >'
-                                    onPress={() => handlePress(p)}
-                                />
-                            </Card>
-                        )
-                    })}
-                </View>
-            </View>
-        </ScrollView>
-    )
+  console.log("render plants")
+  const plants = props.route.params.plants
+  const navigation = props.navigation
+  const area = props.route.params.area
+  console.log(area)
+  const handlePress = (plant) => {
+    return navigation.navigate("PlantDetails", { plant })
+  }
+
+  const handleNewPlantPress = () => {
+    return navigation.navigate("NewPlant", area)
+  }
+  return (
+    <ScrollView>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button title="Menu" onPress={() => navigation.toggleDrawer()} />
+        <Text>
+          These are the plants in your -
+          {area.nickName ? area.nickName : area.type}- garden area
+        </Text>
+
+        <Button
+          icon={<Icon name="code" color="green" />}
+          buttonStyle={{
+            borderRadius: 0,
+            marginLeft: 0,
+            marginRight: 0,
+            marginBottom: 0,
+          }}
+          title="New Plant"
+          onPress={() => handleNewPlantPress()}
+        />
+        <View>
+          {plants.map((p, i) => {
+            return (
+              <Card key={i}>
+                <Text>
+                  <Card.Title>id: {p}</Card.Title>
+                </Text>
+                <Card.Divider />
+                <Image
+                  style={styles.tinyLogo}
+                  source={p.imgURL ? p.imgURL : plantIcon}
+                />
+                <Text style={{ marginBottom: 10 }}></Text>
+                <Button
+                  icon={<Icon name="code" color="green" />}
+                  buttonStyle={{
+                    borderRadius: 0,
+                    marginLeft: 0,
+                    marginRight: 0,
+                    marginBottom: 0,
+                  }}
+                  title="View Plant >"
+                  onPress={() => handlePress(p)}
+                />
+              </Card>
+            )
+          })}
+        </View>
+      </View>
+    </ScrollView>
+  )
 }
 const styles = StyleSheet.create({
-    tinyLogo: {
-        width: 50,
-        height: 50,
-    },
-});
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+})
